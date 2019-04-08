91edd0c6ce2e49ad3e18f23876060118b462349f
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-08 23:00:53 +0700

    mengedit guru_model

--- application/models/Guru_model.php
+++ application/models/Guru_model.php
@@ -2,3 +2,19 @@
 
 class Guru_model extends CI_Model {
 	private $_table = "guru" ;
+
+	public $nig ;
+	public $nama ;
+	public $tgl_lahir ;
+	public $kota_asl;
+	public $gender ;
+	public $alamat ;
+	public $no_telp ;
+	public $password ;
+	public $id_pelajaran ;
+	public $status_user ; 
+
+	public function rules () {
+		
+	}
+}
