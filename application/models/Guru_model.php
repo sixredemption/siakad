c691f987a75594f136eaa455e8922ea1ed5f7ae6
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-08 23:03:31 +0700

    mengedit pada guru_model.php

--- application/models/Guru_model.php
+++ application/models/Guru_model.php
@@ -51,4 +51,20 @@ class Guru_model extends CI_Model {
 		$this->status_user = $post["status_user"] ;
 		$this->db->insert($this->_table , $this) ;
 	}
+
+	public function update() {
+		$post = $this->input->post() ;
+		$this->id = $post["id"] ;
+		$this->nig = $post["nig"] ;
+		$this->nama = $post["nama"] ;
+		$this->tgl_lahir = $post["tgl_lahir"] ;
+		$this->kota_asl = $post["kota_asl"] ;
+		$this->gender = $post["gender"] ;
+		$this->alamat = $post["alamat"] ;
+		$this->no_telp = $post["no_telp"] ;
+		$this->password = $post["password"] ;
+		$this->id_pelajaran = $post["id_pelajaran"] ;
+		$this->status_user = $post["status_user"] ;
+		$this->db->update($this->_table , $this , array('id' => $post['id'])) ;
+	}
 }
