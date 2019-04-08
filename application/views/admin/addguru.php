bde09c554de582070e32d1dfbe47273b11de960b
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-04-08 22:13:53 +0700

    menambahkan tampilan guru

---
+++ application/views/admin/addguru.php
@@ -0,0 +1,129 @@
+<div class="col-sm-8 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">List Guru</li>
+			</ol>
+		</div>
+    <div class="container">
+		<div class="row mt-3">
+			<div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Form Tambah Guru</h1>
+                </div>
+                <div class="card-body">
+                <div class="form-group-input-group">
+			<label for="nis">NIP :</label>
+			<input type="text" class="form-control" id="nis">
+		</div>
+		<div class="form-group">
+			<label for="nama">Nama :</label>
+			<input type="text" class="form-control" id="nama">
+            </div>
+            <div class="form-group">
+			<label for="tanggallahir">Tanggal lahir:</label>
+			<input type="date" class="form-control" id="tanggallahir">
+		</div>
+        <div class="form-group">
+			<label for="kotaasal"> Kota Asal:</label>
+			<input type="text" class="form-control" id="kotaasal">
+		</div>	  
+        <div class="form-group">
+            <label for="alamat">Alamat anda:</label>
+            <textarea type="text" class="form-control" id="alamat"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="Kelas">Mengajar:</label>
+        <select class="form-control">
+            <option value="" disabled selected>Mata Pelajaran</option>
+            <option value="">Bahasa Indonesia</option>
+            <option value="">Bahasa Inggris</option>
+            <option value="">Biologi</option>
+            <option value="">Sejarah</option>
+            <option value="">Kimia</option>
+            <option value="">Geografi</option>
+            <option value="">Fisika</option>
+            <option value="">Ekonomi</option>
+            <option value="">Matematika</option>
+            <option value="">Pendidikan Kewarganegaraan</option>
+            <option value="">Seni Budaya</option>
+            <option value="">Prakarya</option>  
+        </select>
+    </div>
+    <div class="form-group">
+			<label for="nohp"> Nomer Hp:</label>
+			<input type="number" class="form-control" id="kotaasal">
+		</div>	  
+    <div class="form-group">
+      <label for="jk">Jenis Kelamin:</label>
+      <input type="radio" name="gender" value="lakilaki">Laki-Laki
+      <input type="radio" name="gender" value="perempuan">Perempuann
+      </select>
+    </div>
+    <button type="submit" class="btn btn-primary">Simpan</button>
+    <button type="reset" class="btn btn-danger">Reset</button>
+        </div>
+                </div>
+			</div>
+		</div><!--/.row-->
+    </div>
+</div>
+
+
+<!-- <form>
+<div class="form-group-input-group">
+			<label for="nis">NIP :</label>
+			<input type="text" class="form-control" id="nis">
+		</div>
+		<div class="form-group">
+			<label for="nama">Nama :</label>
+			<input type="text" class="form-control" id="nama">
+            </div>
+            <div class="form-group">
+			<label for="tanggallahir">Tanggal lahir:</label>
+			<input type="date" class="form-control" id="tanggallahir">
+		</div>
+        <div class="form-group">
+			<label for="kotaasal"> Kota Asal:</label>
+			<input type="text" class="form-control" id="kotaasal">
+		</div>	  
+        <div class="form-group">
+            <label for="alamat">Alamat anda:</label>
+            <textarea type="text" class="form-control" id="alamat"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="Kelas">Mengajar:</label>
+        <select class="form-control">
+            <option value="" disabled selected>Mata Pelajaran</option>
+            <option value="">Bahasa Indonesia</option>
+            <option value="">Bahasa Inggris</option>
+            <option value="">Biologi</option>
+            <option value="">Sejarah</option>
+            <option value="">Kimia</option>
+            <option value="">Geografi</option>
+            <option value="">Fisika</option>
+            <option value="">Ekonomi</option>
+            <option value="">Matematika</option>
+            <option value="">Pendidikan Kewarganegaraan</option>
+            <option value="">Seni Budaya</option>
+            <option value="">Prakarya</option>  
+        </select>
+    </div>
+    <div class="form-group">
+			<label for="nohp"> Nomer Hp:</label>
+			<input type="number" class="form-control" id="kotaasal">
+		</div>	  
+    <div class="form-group">
+      <label for="jk">Jenis Kelamin:</label>
+      <input type="radio" name="gender" value="lakilaki">Laki-Laki
+      <input type="radio" name="gender" value="perempuan">Perempuann
+      </select>
+    </div>
+    <button type="submit" class="btn btn-primary">Simpan</button>
+    <button type="reset" class="btn btn-danger">Reset</button>
+
+  </div>
+   -->
