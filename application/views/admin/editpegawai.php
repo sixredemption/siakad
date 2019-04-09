be4f31f2ac36ea324be9b27eb948554a7a66995d
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-09 11:12:58 +0700

    mengedit editpegawai.php

---
+++ application/views/admin/editpegawai.php
@@ -0,0 +1,9 @@
+<div class="col-sm-8 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">List Guru</li>
+			</ol>
+		</div>
