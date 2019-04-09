fa6a712d8deadfa61b396217ee4dc6132e28d224
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-09 10:37:11 +0700

    mengedit addpegawai.php

---
+++ application/views/admin/addpegawai.php
@@ -0,0 +1,16 @@
+<div class="col-sm-8 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">List Pegawai</li>
+			</ol>
+		</div>
+    <div class="container">
+		<div class="row mt-3">
+			<div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Form Tambah Pegawai</h1>
+                </div>
