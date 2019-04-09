e61ddfd14c380b1ee1e37463f9300da04140184b
dzikrialifa <dzikrialif99@gmail.com>
2019-04-09 19:23:17 +0700

    File Controllers

---
+++ application/controllers/auth/auth.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+	public function index()
+	{
+		echo "asw";
+    }
+    public function asw(){
+        echo "bapalo :v";
+    }
+}
