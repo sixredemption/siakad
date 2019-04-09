0ee7b97058406d57e3eaea3c737ad87e6f32d6c1
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-09 10:09:15 +0700

    mengedit addguru.php

--- application/views/admin/addguru.php
+++ application/views/admin/addguru.php
@@ -14,29 +14,41 @@
                 <div class="card-header">
                     <h1>Form Tambah Guru</h1>
                 </div>
-                <div class="card-body">
-                <div class="form-group-input-group">
+        <div class="card-body">
+        <div class="form-group-input-group">
 			<label for="nig">NIG :</label>
 			<input type="text" class="form-control" id="nig">
 		</div>
+
 		<div class="form-group">
 			<label for="nama">Nama :</label>
 			<input type="text" class="form-control" id="nama">
-            </div>
-            <div class="form-group">
+		</div>
+			
+        <div class="form-group">
 			<label for="tgl_lahir">Tanggal lahir:</label>
 			<input type="date" class="form-control" id="tgl_lahir">
 		</div>
+
         <div class="form-group">
 			<label for="kota_asl"> Kota Asal:</label>
 			<input type="text" class="form-control" id="kota_asl">
-		</div>	  
+		</div>
+
+		<div class="form-group">
+      		<label for="gender">Jenis Kelamin:</label>
+      		<input type="radio" name="gender" value="lakilaki">Laki-Laki
+      		<input type="radio" name="gender" value="perempuan">Perempuann
+      		</select>
+    	</div>	 
+
         <div class="form-group">
             <label for="alamat">Alamat anda:</label>
             <textarea type="text" class="form-control" id="alamat"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="Kelas">Mengajar:</label>
+		</div>
+		
+        <!-- <div class="form-group">
+            <label for="status_user">Mengajar:</label>
         <select class="form-control">
             <option value="" disabled selected>Mata Pelajaran</option>
             <option value="">Bahasa Indonesia</option>
@@ -52,17 +64,34 @@
             <option value="">Seni Budaya</option>
             <option value="">Prakarya</option>  
         </select>
-    </div>
-    <div class="form-group">
-			<label for="nohp"> Nomer Hp:</label>
-			<input type="number" class="form-control" id="kota_asl">
-		</div>	  
-    <div class="form-group">
-      <label for="jk">Jenig Kelamin:</label>
-      <input type="radio" name="gender" value="lakilaki">Laki-Laki
-      <input type="radio" name="gender" value="perempuan">Perempuann
-      </select>
-    </div>
+	</div> -->
+	
+    	<div class="form-group">
+			<label for="no_telp"> Nomor Telepon :</label>
+			<input type="number" class="form-control" id="no_telp">
+		</div>
+
+		<div class="form-group">
+			<label for="password"> Password:</label>
+			<input type="text" class="form-control" id="password">
+		</div>
+
+		<div class="form-group">
+			<label for="id_pelajaran"> Mengajar:</label>
+			<input type="number" class="form-control" id="id_pelajaran">
+		</div>
+
+		<div class="form-group">
+            <label for="status_user">Status User:</label>
+            <select class="form-control" name="status_user">
+        	<option value="" disabled selected>Pilih User</option>
+			<option value="1">Siswa</option>
+			<option value="2">Guru</option>
+			<option value="3">Pegawai</option>
+		</select>
+			</div>
+
+
     <button type="submit" class="btn btn-primary">Simpan</button>
     <button type="reset" class="btn btn-danger">Reset</button>
         </div>
@@ -95,7 +124,7 @@
             <textarea type="text" class="form-control" id="alamat"></textarea>
         </div>
         <div class="form-group">
-            <label for="Kelas">Mengajar:</label>
+            <label for="status_user">Mengajar:</label>
         <select class="form-control">
             <option value="" disabled selected>Mata Pelajaran</option>
             <option value="">Bahasa Indonesia</option>
