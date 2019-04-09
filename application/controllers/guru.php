f3d4be3503ce5f19d69ff1bbe3d0f191557dcd24
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-09 11:24:24 +0700

    mengedit guru.php

--- application/controllers/guru.php
+++ application/controllers/guru.php
@@ -27,4 +27,29 @@ class guru extends CI_Controller {
 
 		$this->load->view("admin/tambahguru") ;
 	}
+	public function edit($id = null) {
+		if (!isset($id)) redirect ('guru/guru_view') ;
+
+		$guru = $this->Guru_model ;
+		$validation = $this->form_validation ;
+		$validation->set_rules($guru->rules()) ;
+
+		if ($validation->run()) {
+			$guru->update() ;
+			$this->session->set_flashdata('succes' , 'Berhasil disimpan') ;
+		}
+
+		$data["guru"] = $guru->getById($id) ;
+		if (!$data["product"]) show_404() ;
+
+		$this->load->view("guru/guru_view/edit_form" , $data) ;
+	}
+
+	public function delete($id = null) {
+		if (!isset($id)) show_404() ;
+
+		if ($this->Guru_model->delete($id)) {
+			redirect(site_url('admin/guru')) ;
+		}
+	}
 }
