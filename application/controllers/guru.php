f1e313e928641fa68b5ea727b7afc2b298c3edb6
dzikrialifa <dzikrialif99@gmail.com>
2019-04-09 12:03:17 +0700

    File Configuration CodeIgniter

--- application/controllers/guru.php
+++ application/controllers/guru.php
@@ -1,53 +1,60 @@
-<?php 
-class guru extends CI_Controller{
+<?php
+class guru extends CI_Controller
+{
 
-public function index(){
-$this->load->view('template/template_guru/header' );
-$this->load->view('template/template_guru/sidebar'); 
-$this->load->view('guru/home');
-$this->load->view('template/template_guru/footer'); 
-}
-
-public function biodata(){
-$this->load->view('template/template_guru/header' ); 
-$this->load->view('template/template_guru/sidebar');
-$this->load->view('guru/biodata');
-$this->load->view('template/template_guru/footer'); 
-}
-
-public function isinilai(){
-$this->load->view('template/template_guru/header' );
-$this->load->view('template/template_guru/sidebar'); 
-$this->load->view('guru/isinilai');
-$this->load->view('template/template_guru/footer'); 
-}
-
-public function jadwal(){
-$this->load->view('template/template_guru/header' );
-$this->load->view('template/template_guru/sidebar');
-$this->load->view('guru/jadwal'); 
-$this->load->view('template/template_guru/footer'); 
-}
-
-public function nilai(){
-$this->load->view('template/template_guru/header' ); 
-$this->load->view('template/template_guru/sidebar');
-$this->load->view('guru/nilai');
-$this->load->view('template/template_guru/footer'); 
-}
-
-public function tambahnilai(){
-    $this->load->view('template/template_guru/header' );
-    $this->load->view('template/template_guru/sidebar'); 
-    $this->load->view('guru/tambahnilai');
-    $this->load->view('template/template_guru/footer'); 
-}
-
-public function editbiodata(){
-    $this->load->view('template/template_guru/header' );
-    $this->load->view('template/template_guru/sidebar'); 
-    $this->load->view('guru/editbiodata');
-    $this->load->view('template/template_guru/footer'); 
+    public function index()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/home');
+        $this->load->view('template_guru/footer');
     }
 
+    public function biodata()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/biodata');
+        $this->load->view('template_guru/footer');
+    }
+
+    public function isinilai()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/isinilai');
+        $this->load->view('template_guru/footer');
+    }
+
+    public function jadwal()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/jadwal');
+        $this->load->view('template_guru/footer');
+    }
+
+    public function nilai()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/nilai');
+        $this->load->view('template_guru/footer');
+    }
+
+    public function tambahnilai()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/tambahnilai');
+        $this->load->view('template_guru/footer');
+    }
+
+    public function editbiodata()
+    {
+        $this->load->view('template_guru/header');
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/editbiodata');
+        $this->load->view('template_guru/footer');
+    }
 }
