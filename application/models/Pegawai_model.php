df92ee1403b3587f02606f684ea5049eadf23cb7
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-09 09:46:10 +0700

    mengedit pada pegawai_model.php

--- application/models/Pegawai_model.php
+++ application/models/Pegawai_model.php
@@ -37,4 +37,12 @@ class Pegawai_model extends CI_Model {
 		$this->gender = $post["gender"] ;
 		$this->db->insert($this->_table , $this) ;
 	}
+	public function update() {
+		$post = $this->input->post() ;
+		$this->nip = $post["nip"] ;
+		$this->nama = $post["nama"] ;
+		$this->password = $post["password"] ;
+		$this->gender = $post["gender"] ;
+		$this->db->insert($this->_table , $this , array('id' => $post['id'])) ;
+	}
 }
