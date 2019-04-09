dc6ae9b159aa4e281c33a50e4bc151d90d40bc17
dzikrialifa <dzikrialif99@gmail.com>
2019-04-09 23:24:36 +0700

    Templating Auth Login

---
+++ application/views/auth/login.php
@@ -0,0 +1,49 @@
+<body class="bg-gradient-primary">
+
+    <div class="container mt-5">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-5">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h2 text-gray-900 mb-4"><i class="fas fa-users"></i> SIAKAD LOGIN </h1>
+                                    </div>
+                                    <?= $this->session->flashdata('message'); ?>
+                                    <form class="user" method="post" action="<?= base_url('auth/index'); ?>">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="username" placeholder="Username" name="username" value="<?= set_value('username'); ?>">
+                                            <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
+                                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+
+                                        </div>
+
+                                        <button type="submit" class="btn btn-success btn-user btn-block">
+                                            Login
+                                        </button>
+                                    </form>
+                                    <div class="text-center mt-3">
+                                        <a class="small" href="<?= base_url('register') ?>">Login Guru ?</a> or
+                                        <a class="small" href="<?= base_url(); ?>">Back Home</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
