ff27cef0acaf5286c7c27b335998f0d563d55183
dzikrialifa <dzikrialif99@gmail.com>
2019-04-09 18:09:52 +0700

    Edit Footer

--- application/views/template_home/footer.php
+++ application/views/template_home/footer.php
@@ -2,7 +2,7 @@
     <div class="footer-area">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="col-md-6 col-sm-4 col-xs-12">
                     <div class="footer-content">
                         <div class="footer-head">
                             <div class="footer-logo">
@@ -31,7 +31,7 @@
                     </div>
                 </div>
                 <!-- end single footer -->
-                <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="col-md-6 col-sm-4 col-xs-12">
                     <div class="footer-content">
                         <div class="footer-head">
                             <h4>information</h4>
@@ -42,22 +42,6 @@
                                 <p><span>Tel:</span> +123 456 789</p>
                                 <p><span>Email:</span> contact@example.com</p>
                                 <p><span>Working Hours:</span> 9am-5pm</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- end single footer -->
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <div class="footer-content">
-                        <div class="footer-head">
-                            <h4>Instagram</h4>
-                            <div class="flicker-img">
-                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/1.jpg" alt=""></a>
-                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/2.jpg" alt=""></a>
-                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/3.jpg" alt=""></a>
-                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/4.jpg" alt=""></a>
-                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/5.jpg" alt=""></a>
-                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/6.jpg" alt=""></a>
                             </div>
                         </div>
                     </div>
