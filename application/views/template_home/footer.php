4bce727e5c316375cecbbfc2b53ec5cce22d3c44
dzikrialifa <dzikrialif99@gmail.com>
2019-04-09 17:55:57 +0700

    Templating Landing Page

---
+++ application/views/template_home/footer.php
@@ -0,0 +1,114 @@
+<footer>
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-head">
+                            <div class="footer-logo">
+                                <h2><span>e</span>Business</h2>
+                            </div>
+
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
+                            <div class="footer-icons">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-facebook"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-twitter"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-google"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single footer -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-head">
+                            <h4>information</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                            </p>
+                            <div class="footer-contacts">
+                                <p><span>Tel:</span> +123 456 789</p>
+                                <p><span>Email:</span> contact@example.com</p>
+                                <p><span>Working Hours:</span> 9am-5pm</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single footer -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-head">
+                            <h4>Instagram</h4>
+                            <div class="flicker-img">
+                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/1.jpg" alt=""></a>
+                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/2.jpg" alt=""></a>
+                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/3.jpg" alt=""></a>
+                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/4.jpg" alt=""></a>
+                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/5.jpg" alt=""></a>
+                                <a href="#"><img src="<?= base_url('assets_home/') ?>img/portfolio/6.jpg" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-area-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="copyright text-center">
+                        <p>
+                            &copy; Copyright <strong>eBusiness</strong>. All Rights Reserved
+                        </p>
+                    </div>
+                    <div class="credits">
+                        <!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
+              -->
+                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                        Re-distributed by <a href="https://themewagon.com/" target="_blank">Themewagon</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+<!-- JavaScript Libraries -->
+<script src="<?= base_url('assets_home/') ?>lib/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/venobox/venobox.min.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/knob/jquery.knob.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/wow/wow.min.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/parallax/parallax.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/easing/easing.min.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+<script src="<?= base_url('assets_home/') ?>lib/appear/jquery.appear.js"></script>
+<script src="<?= base_url('assets_home/') ?>lib/isotope/isotope.pkgd.min.js"></script>
+
+<!-- Contact Form JavaScript File -->
+<script src="<?= base_url('assets_home/') ?>contactform/contactform.js"></script>
+
+<script src="<?= base_url('assets_home/') ?>js/main.js"></script>
+</body>
+
+</html>
