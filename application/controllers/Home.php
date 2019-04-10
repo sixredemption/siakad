eabcff5e73ae34c2f09c0f55b04e751244945cab
dzikrialifa <dzikrialif99@gmail.com>
2019-04-10 23:33:53 +0700

    Edit Controllers Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,12 +13,12 @@ class Home extends CI_Controller
 	{
 		$data['judul'] = 'SMAN 4 MACIPO';
 
-		//$this->db->order_by('tanggal', 'DESC');
-		//$data['user']	=	$this->db->get($this->_tblpengumuman)->result();
+		$this->db->order_by('tanggal', 'DESC');
+		$data['pengumuman']	=	$this->db->get($this->_tblpengumuman)->result();
 
 		$this->load->view('template_home/header', $data);
 		$this->load->view('template_home/navbar');
-		$this->load->view('template_home/index');
+		$this->load->view('template_home/index', $data);
 		$this->load->view('template_home/footer');
 	}
 }
