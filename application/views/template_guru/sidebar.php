e1f2176c683bea750a61182a9fa9957b38361850
debyprast <debyprast18@gmail.com>
2019-04-10 19:15:56 +0700

    pembaruan sidebar membuat logout

--- application/views/template_guru/sidebar.php
+++ application/views/template_guru/sidebar.php
@@ -4,6 +4,25 @@
         <div class="navbar-header">
           <a class="navbar-brand" href="<?= base_url();?>guru/index"> SMA 4 MACIPO</a>
         <img src="<?php echo base_url() ?>1.jpg" width="50px">
+        </div>
+        
+        <ul class="nav navbar-nav navbar-right">
+					<li class="dropdown dropdown-user">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+						<img  src="<?php echo base_url() ?>3.jpg" class="img-circle" width="40px" height="40px>						
+            <span class="username username-hide-on-mobile">DEBY D.P </span>
+						<i class="fa fa-angle-down"></i>
+						</a>
+						<ul class="dropdown-menu dropdown-menu-default">
+							<li>
+              <a href="<?= base_url();?>guru/password"><i class="fa fa-gear">Ganti Password</i></a>
+              </li>
+              <li>
+              <a href="<?= base_url();?>guru/index"><i class="fa fa-power-off">Log Out </i></a> 
+							</li>
+						</ul>
+					</li>
+				</ul>
         </nav>
 
       <aside class="sidebar">
