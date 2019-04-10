7be65c4bf668120bcf7e77ebcbbf3f508e1ea74a
debyprast <debyprast18@gmail.com>
2019-04-10 19:27:11 +0700

    buat class ganti password dan penambahan controller

--- application/controllers/guru.php
+++ application/controllers/guru.php
@@ -50,4 +50,10 @@ public function editbiodata(){
     $this->load->view('template_guru/footer'); 
     }
 
+    public function password(){
+        $this->load->view('template_guru/header' ); 
+        $this->load->view('template_guru/sidebar');
+        $this->load->view('guru/password');
+        $this->load->view('template_guru/footer'); 
+        }
 }
---
+++ application/views/guru/password.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <section class="content">
+    <div class="jumbotron text-center"> 
+      <h3> Ganti Password </h3>
+      </section> 
+    </div>
+
+    <div class="col-md-12">
+        <section class="content">
+        <div class="panel panel-default">
+        <div class="panel-heading">
+            <i class="fa fa-external-link-square"></i> Password
+        </div>  
+        <div class="container">
+		<div class="row mt-3">
+			<div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Ganti Password</h1>
+                </div>
+                <div class="card-body">
+        <div class="form-group-input-group">
+			<label for="nis">Password Lama </label>
+			<input type="text" class="form-control" id="nis">
+		</div>
+		<div class="form-group">
+			<label for="nama">Password Baru</label>
+			<input type="text" class="form-control" id="nama">
+        </div>
+        <div class="form-group">
+			<label for="nama">Konfirmasi Password Baru</label>
+			<input type="text" class="form-control" id="nama">
+        </div>
+            
+    <a <button type="submit" class="btn btn-primary" href="<?= base_url();?>guru/index">Simpan</a></button>
+    <a <button type="reset" class="btn btn-danger" href="<?= base_url();?>guru/index">Kembali</a></button>
+        </div>
+                </div>
+			</div>
+</fieldset>
+</form>
+</body>
+</section>
+</div>
