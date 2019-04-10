fbb0b891cf72d61f585ef7e5a055ccb968781819
dzikrialifa <dzikrialif99@gmail.com>
2019-04-10 17:47:50 +0700

    Edit Login Model

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -5,10 +5,14 @@ class Login_model extends CI_Model
     function cek_login($user, $pass)
     {
         // return $this->db->get_where($tabel, $where);
+        $u = htmlspecialchars($user);
+        $p = md5($pass);
+        $us = $this->db->escape_str($u);
+        $ps = $this->db->escape_str($p);
         $query = $this->db->select('*')
             ->from('guru')
-            ->where('username', $user)
-            ->where('password', $pass)
+            ->where('username', $us)
+            ->where('password', $ps)
             ->limit(1)
             ->get();
         if ($query->num_rows() == 1) {
