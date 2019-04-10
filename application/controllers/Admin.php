17f0f0d39702b1510f77533b6ed605fb1e341bc3
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-10 13:15:54 +0700

    mengedit Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -65,7 +65,7 @@
     public function listpegawai(){
 		$data["pegawai"] = $this->Pegawai_model->getAll() ;
         $this->load->view('template_admin/header');
-        $this->load->view('admin/listpegawai');
+        $this->load->view('admin/listpegawai' , $data);
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
