dde4d4a93399eabf3cbe80c431e1f1dd4d41597c
dzikrialifa <dzikrialif99@gmail.com>
2019-04-10 18:00:45 +0700

    Edit Login Model

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -10,7 +10,7 @@ class Login_model extends CI_Model
         $us = $this->db->escape_str($u);
         $ps = $this->db->escape_str($p);
         $query = $this->db->select('*')
-            ->from('guru')
+            ->from('admin')
             ->where('username', $us)
             ->where('password', $ps)
             ->limit(1)
