8c56b3f2bdffecc78f160a70c3640dac5b9b4a37
dzikrialifa <dzikrialif99@gmail.com>
2019-04-10 17:34:45 +0700

    Edit Controller Admin & Auth

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,85 +1,108 @@
 <?php
- class Admin extends CI_Controller{
-    public function index(){
-        $data['judul']="Wellcome To Administrator";
-        $this->load->view('template_admin/header',$data);
+class Admin extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        if ($this->session->userdata('status') != "login") {
+            redirect(base_url('login'));
+        }
+    }
+
+    public function index()
+    {
+        $data['judul'] = "Wellcome To Administrator";
+        $this->load->view('template_admin/header', $data);
         $this->load->view('admin/dashboard');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function pengumuman(){
+    public function pengumuman()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/pengumuman');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function nilaisiswaipa(){
+    public function nilaisiswaipa()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/nilaisiswaipa');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
 
-    public function daftarsiswaspp(){
+    public function daftarsiswaspp()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/daftarsiswaspp');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
 
-    public function nilaisiswaips(){
+    public function nilaisiswaips()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/nilaisiswaips');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    
-    public function statusspp(){
+
+    public function statusspp()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/statusspp');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function listguru(){
+    public function listguru()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/listguru');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function listsiswa(){
+    public function listsiswa()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/listsiswa');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function listadmin(){
+    public function listadmin()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/listadmin');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function addguru(){
+    public function addguru()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/addguru');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function addsiswa(){
+    public function addsiswa()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/addsiswa');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function editguru(){
+    public function editguru()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/editguru');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
-    public function uploadjadwal(){
+    public function uploadjadwal()
+    {
         $this->load->view('template_admin/header');
         $this->load->view('admin/jadwal');
         $this->load->view('template_admin/sidebar');
         $this->load->view('template_admin/footer');
     }
- }
+}
--- application/controllers/auth/auth.php
+++ application/controllers/auth/auth.php
@@ -3,10 +3,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-	private $_table = "pengumuman";
+	private $_guru = "guru";
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('login_model');
 	}
 
 	public function index()
@@ -15,5 +16,33 @@ class Auth extends CI_Controller
 		$this->load->view('auth/header', $data);
 		$this->load->view('auth/login');
 		$this->load->view('auth/footer');
+	}
+
+	public function login_admin()
+	{
+		$username	=	$this->input->post('username');
+		$password	=	$this->input->post('password');
+
+		$cek		= $this->login_model->cek_login($username, $password);
+		if ($cek) {
+			// DATANYA ADA
+			foreach ($cek as $row) {
+				$this->session->set_userdata('nig', $row->nig);
+				$this->session->set_userdata('username', $row->username);
+				redirect(base_url("admin"));
+			}
+			// $data = array(
+			// 	'nig' => $username,
+			// 	'nama' => 
+			// redirect(base_url("admin"));
+		} else {
+			echo "user pass salah";
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect(base_url("login"));
 	}
 }
