dd5dcaa071edb4391af296688927f4809bd29112
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-10 09:33:41 +0700

    mengedit Guru_model.php

--- application/models/Guru_model.php
+++ application/models/Guru_model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed') ;
 
-class Guru_model extends CI_Model {
+class M_guru extends CI_Model {
 	private $_table = "guru" ;
 
 	public $nig ;
