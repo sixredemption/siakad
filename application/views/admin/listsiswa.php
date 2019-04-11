c5f4eafd0fe0f3d8d293d61f45943d1791c419bb
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-04-11 11:38:46 +0700

    Melakukan editing pada halaman daftar siswa

--- application/views/admin/listsiswa.php
+++ application/views/admin/listsiswa.php
@@ -1,4 +1,122 @@
-  <div class="col-sm-8 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+      <div class="row">
+        <ol class="breadcrumb">
+          <li><a href="#">
+            <em class="fa fa-home"></em>
+          </a></li>
+          <li class="active">Pengumuman</li>
+        </ol>
+	  </div>
+	  <div class="container-fluid" style="margin-top:40px">	  
+	  <form method="post"  action="<?php  echo base_url(). 'admin/addsiswa'; ?>">
+			<button class="btn btn-primary" id="submit-buttons" type="submit" ​​​​​>Tambah Siswa</button>
+		</form><br>   
+
+	  <table class="table table-striped">
+				<thead>
+					<tr>
+										<th>Nis</th>
+                    <th>Nama</th>
+                    <th>Tanggal Lahir</th>
+                    <th>Kota Asal</th>
+                    <th>Gender</th>
+                    <th>Alamat</th>
+                    <th>Jurusan</th>
+                    <th>Kelas</th>
+                    <th>No Telepon</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                    </tr>
+  			<tbody>
+								<th scope='row'>17271721721</th>
+										<td>KAnjeng</td>
+										<td>1998-12-12</td>
+										<td>Zimbawe</td>
+										<td>Laki-Laki</td>
+										<td>Jl.Zimbawe 5</td>
+										<td>Ipa</td>
+										<td>Ipa2</td>
+										<td>084818121</td>
+										<td>Siswa</td>
+										<td>
+										<button class="btn btn-success fa fa-edit"></button>
+										<button class="btn btn-danger fa fa-trash"></button>
+										</td>
+							</tbody>
+			</table>
+			</div>
+			<div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <div class="col-sm-8 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 		<div class="row">
 			<ol class="breadcrumb">
 				<li><a href="#">
@@ -15,14 +133,13 @@
             </div>
 			</div>
 		</div>
-        <!--/.row-->
 		<div class= container>
         <div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header">Daftar Siswa</h1>
             <div class="panel-button-tab-left">
-        	<button class="btn btn-"> </button>
-            </div>
+			
+         </div>
 				<table class="table table-striped">
 				<thead>
 					<tr>
@@ -56,8 +173,13 @@
 			</tbody>
 	        </table>
 			</div>
+			<div>
+		<form method="post"  action="<?php  echo base_url(). 'tombol/link'; ?>">
+			<button id="submit-buttons" type="submit" ​​​​​>Submit 1</button>
+		</form>   
+	</div>
 		</div>
 </div>
 	
         
-       
+        -->
