5ed3de43b8cb1c79d7ef17e6dc98438327c212b7
dzikrialifa <dzikrialif99@gmail.com>
2019-04-11 20:03:28 +0700

    Edit Controller

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '178.128.110.239',
+	'username' => 'dzikri',
+	'password' => '123456',
 	'database' => 'siakadsma',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
