7ed5b79755162d1683e312ddb8d6fb00e0ba4e93
dzikrialifa <dzikrialif99@gmail.com>
2019-04-11 09:32:40 +0700

    Edit Controllers Siswa & Auth Login

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -4,12 +4,9 @@ class Siswa extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        // if ($this->session->userdata('status') != "login") {
-        //     redirect(base_url('login'));
-        // }
         $this->load->model('login_model');
         if (empty($this->session->userdata('nisn')) and empty($this->session->userdata('password'))) {
-            redirect(base_url('login_siswa'));
+            redirect(base_url('login'));
         }
     }
     public function index()
--- application/controllers/auth/auth.php
+++ application/controllers/auth/auth.php
@@ -6,6 +6,7 @@ class Auth extends CI_Controller
 	private $_guru 	=	"guru";
 	private $_admin	=	"admin";
 	private $_siswa	=	"siswa";
+
 	public function __construct()
 	{
 		parent::__construct();
@@ -14,16 +15,30 @@ class Auth extends CI_Controller
 	}
 	public function index()
 	{
-		$data['judul']	=	'Login Siswa';
+		redirect(base_url('login'));
+	}
+
+	public function admin() // VIEW LOGIN ADMIN
+	{
+		$data['judul']	=	'Login Admin';
 		$this->load->view('auth/header', $data);
 		$this->load->view('auth/login');
 		$this->load->view('auth/footer');
 	}
-	public function siswa()
+
+	public function siswa() // VIEW LOGIN SISWA
 	{
 		$data['judul']	=	'Login Siswa';
 		$this->load->view('auth/header', $data);
 		$this->load->view('auth/login_siswa');
+		$this->load->view('auth/footer');
+	}
+
+	public function guru() // VIEW LOGIN GURU
+	{
+		$data['judul']	=	'Login Guru';
+		$this->load->view('auth/header', $data);
+		$this->load->view('auth/login_guru');
 		$this->load->view('auth/footer');
 	}
 
@@ -32,24 +47,43 @@ class Auth extends CI_Controller
 		$username	=	$this->input->post('username');
 		$password	=	$this->input->post('password');
 		$where 		=	"username";
-		$cek		= $this->login_model->cek_login($this->_admin, $where, $username, $password);
+		$cek		= 	$this->login_model->cek_login($this->_admin, $where, $username, $password);
 		if ($cek) {
 			foreach ($cek as $row) {
 				$this->session->set_userdata('username', $row->username);
-				redirect(base_url("admin"));
+				redirect(base_url("admin")); // localhost/controllerAdmin
 			}
 		} else {
-			$this->index();
+			$this->admin();
+		}
+	}
+
+	public function login_guru()
+	{
+		$nig		=	$this->input->post('nig');
+		$password	=	$this->input->post('password');
+		$where 		=	"nig";
+		$cek		= 	$this->login_model->cek_login($this->_guru, $where, $nig, $password);
+		if ($cek) {
+			// DATANYA ADA
+			foreach ($cek as $row) {
+				$this->session->set_userdata('nig', $row->nig);
+				$this->session->set_userdata('nama_siswa', $row->nama_siswa);
+				$this->session->set_userdata('jenis_kelamin', $row->jenis_kelamin);
+				$this->session->set_userdata('username', $row->username);
+				redirect(base_url("siswa")); // localhost/controllerSiswa
+			}
+		} else {
+			$this->guru();
 		}
 	}
 
 	public function login_siswa()
 	{
-		$nisn	=	$this->input->post('nisn');
+		$nisn		=	$this->input->post('nisn');
 		$password	=	$this->input->post('password');
 		$where 		=	"nisn";
-
-		$cek		= $this->login_model->cek_login($this->_siswa, $where, $nisn, $password);
+		$cek		= 	$this->login_model->cek_login($this->_siswa, $where, $nisn, $password);
 		if ($cek) {
 			// DATANYA ADA
 			foreach ($cek as $row) {
@@ -57,12 +91,13 @@ class Auth extends CI_Controller
 				$this->session->set_userdata('nama_siswa', $row->nama_siswa);
 				$this->session->set_userdata('jenis_kelamin', $row->jenis_kelamin);
 				$this->session->set_userdata('username', $row->username);
-				redirect(base_url("siswa"));
+				redirect(base_url("siswa")); // localhost/controllerSiswa
 			}
 		} else {
 			$this->siswa();
 		}
 	}
+
 	public function logout()
 	{
 		$this->session->sess_destroy();
