e10dc5facd33bccd450f2095677ea577a373e671
dzikrialifa <dzikrialif99@gmail.com>
2019-04-11 19:39:23 +0700

    Edit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'siakad.kodeskillet.com';
+$config['base_url'] = 'http://siakad.kodeskillet.com';
 
 /*
 |--------------------------------------------------------------------------
