322744215ce7dd146a46d8d0fc3d7af14d451abb
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-04-11 23:34:19 +0700

    home siswa

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,9 +6,18 @@ class Home extends CI_Controller
 
 	public function index()
 	{
+		$data['judul'] = 'SMAN 4 MACIPO';
 
+		$this->db->order_by('tanggal', 'DESC');
+		$this->db->limit(6);
+		$data['pengumuman']	=	$this->db->get($this->_tblpengumuman)->result();
+
+		$this->load->view('template_home/header', $data);
+		$this->load->view('template_home/navbar');
+		$this->load->view('template_home/index', $data);
+		$this->load->view('template_home/footer');
 
 		
-		$this->load->view('template_home/index');
+		//$this->load->view('template_home/index');
 	}
 }
