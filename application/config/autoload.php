9977b2baf1ff2a3180cb3c04518aef4de611e3fc
dzikrialifa <dzikrialif99@gmail.com>
2019-04-12 20:29:46 +0700

    Edit Autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'file', 'url_helper');
+$autoload['helper'] = array('url', 'form', 'file', 'url_helper', 'tanggal_indo');
 
 /*
 | -------------------------------------------------------------------
