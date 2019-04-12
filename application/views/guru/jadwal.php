61775261c3ff91eb11619471d800632e64058ae4
dzikrialifa <dzikrialif99@gmail.com>
2019-04-12 15:09:58 +0700

    Edit Templating Guru

--- application/views/guru/jadwal.php
+++ application/views/guru/jadwal.php
@@ -2,13 +2,19 @@
 <section class="content">
 
 <div class="jumbotron text-center"> 
-      <h3> Lihat Jadwal </h3>
-      <div class="panel panel-default">
-        <div class="panel-heading">
-            <i class="fa fa-external-link-square">Download Jadwal</i> 
-        </div>
+  <h3> Lihat Jadwal </h3>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+          <i class="fa fa-external-link-square">Download Jadwal</i> 
+      </div>
 
         <div class="panel-body">
-        <button type="edit" name="edit" class="btn btn-primary" >Download jadwal</button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPA Kelas 10</i></button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPS Kelas 10</i></button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPA Kelas 11</i></button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPS Kelas 11</i></button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPA Kelas 12</i></button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPS Kelas 12</i></button>
+        </div>
 </section> 
 </div>
