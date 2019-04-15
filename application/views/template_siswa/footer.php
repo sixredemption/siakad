9c0fbcfb729d2419f2bbc09a7770245b18669a88
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-04-16 00:45:11 +0700

    mengganti template assets siswa

--- application/views/template_siswa/footer.php
+++ application/views/template_siswa/footer.php
@@ -1,6 +1,44 @@
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
-   
+    -->
+
+	<html>
+<body>
+
+    <script src="<?= base_url('assets/') ?>vendors/jquery/dist/jquery.min.js"></script>
+    <script src="<?= base_url('assets/') ?>vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="<?= base_url('assets/') ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= base_url('assets/') ?>assets/js/main.js"></script>
+
+
+    <script src="<?= base_url('assets/') ?>vendors/chart.js/dist/Chart.bundle.min.js"></script>
+    <script src="<?= base_url('assets/') ?>assets/js/dashboard.js"></script>
+    <script src="<?= base_url('assets/') ?>assets/js/widgets.js"></script>
+    <script src="<?= base_url('assets/') ?>vendors/jqvmap/dist/jquery.vmap.min.js"></script>
+    <script src="<?= base_url('assets/') ?>vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <script src="<?= base_url('assets/') ?>vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script>
+        (function($) {
+            "use strict";
+
+            jQuery('#vmap').vectorMap({
+                map: 'world_en',
+                backgroundColor: null,
+                color: '#ffffff',
+                hoverOpacity: 0.7,
+                selectedColor: '#1de9b6',
+                enableZoom: true,
+                showTooltip: true,
+                values: sample_data,
+                scaleColors: ['#1de9b6', '#03a9f5'],
+                normalizeFunction: 'polynomial'
+            });
+        })(jQuery);
+    </script>
+
+</body>
+
+</html>
--- application/views/template_siswa/sidebar.php
+++ application/views/template_siswa/sidebar.php
@@ -1,4 +1,4 @@
-<body>
+<!-- <body>
     <div class="wrapper">
       <nav class="navbar navbar-default">
         <div class="navbar-header">
@@ -23,4 +23,87 @@
           </ul>
         </menu>
       </aside>
-    </div>
+    </div> -->
+
+		
+<body>
+
+
+<!-- Left Panel -->
+
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+
+        <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+             </div>
+
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                </li>
+                <menu>
+          <<li><a href="<?= base_url();?>siswa/index"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="<?= base_url();?>siswa/nilai"><i class="fa fa-bookmark-o"></i> Nilai Siswa</a></li>
+						<li><a href="<?= base_url();?>siswa/jadwal"><i class="fa fa-bookmark-o"></i>Jadwal Siswa</a></li>
+            <li><a href="<?= base_url();?>siswa/pengumuman"><i class="fa fa-bell"></i> Pengumuman</a></li>
+            <li><a href="<?= base_url();?>siswa/statusspp"><i class="fa fa-money"></i>Status Pembayaran Spp</a></li>
+						<li><a href="<?= base_url();?>logout/siswa"><i class="fa fa-sign-out"></i> Logout </a></li>
+          </ul>
+        </menu>
+    
+</aside><!-- /#left-panel -->
+
+<header id="header" class="header">
+
+            <div class="header-menu">
+
+                <div class="col-sm-7">
+                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                    
+
+                        
+                        
+                <div class="col-sm-5">
+                    <div class="user-area dropdown float-right show">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            <img  src="<?php echo base_url() ?>3.jpg" class="user-avatar rounded-circle" alt="User Avatar">
+                        </a>
+
+                        <div class="user-menu dropdown-menu show" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-88px, 0px, 0px);">
+                        <li>
+              <a href="<?= base_url();?>siswa/password"><i class="fa fa-gear">Ganti Password</i></a>
+              </li>
+              <li>
+              <a href="<?= base_url();?>siswa/index"><i class="fa fa-power-off">Log Out </i></a> 
+							</li>
+                        </div>
+                    </div>
+
+                    <div class="language-select dropdown" id="language-select">
+                        <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="language" aria-haspopup="true" aria-expanded="true">
+                            <i class="flag-icon flag-icon-us"></i>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="language">
+                            <div class="dropdown-item">
+                                <span class="flag-icon flag-icon-fr"></span>
+                            </div>
+                            <div class="dropdown-item">
+                                <i class="flag-icon flag-icon-es"></i>
+                            </div>
+                            <div class="dropdown-item">
+                                <i class="flag-icon flag-icon-us"></i>
+                            </div>
+                            <div class="dropdown-item">
+                                <i class="flag-icon flag-icon-it"></i>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </header>
