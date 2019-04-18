38d75b8a9fb7765f739834425be799d52cbf9c48
debyprast <debyprast18@gmail.com>
2019-04-18 15:53:24 +0700

    memperbaiki footer>>data yang diambil

--- application/views/template_guru/footer.php
+++ application/views/template_guru/footer.php
@@ -4,12 +4,12 @@
     <script src="<?= base_url('assets_guru/') ?>vendors/jquery/dist/jquery.min.js"></script>
     <script src="<?= base_url('assets_guru/') ?>vendors/popper.js/dist/umd/popper.min.js"></script>
     <script src="<?= base_url('assets_guru/') ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="<?= base_url('assets_guru/') ?>js/main.js"></script>
+    <script src="<?= base_url('assets_guru/') ?>assets/js/main.js"></script>
 
 
     <script src="<?= base_url('assets_guru/') ?>vendors/chart.js/dist/Chart.bundle.min.js"></script>
-    <script src="<?= base_url('assets_guru/') ?>js/dashboard.js"></script>
-    <script src="<?= base_url('assets_guru/') ?>js/widgets.js"></script>
+    <script src="<?= base_url('assets_guru/') ?>assets/js/dashboard.js"></script>
+    <script src="<?= base_url('assets_guru/') ?>assets/js/widgets.js"></script>
     <script src="<?= base_url('assets_guru/') ?>vendors/jqvmap/dist/jquery.vmap.min.js"></script>
     <script src="<?= base_url('assets_guru/') ?>vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
     <script src="<?= base_url('assets_guru/') ?>vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
