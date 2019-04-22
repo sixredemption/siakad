0807c6842195c32c189ff9df5add199fca049d6a
KhosyAkmal <akmalkhosy@gmail.com>
2019-04-22 22:41:14 +0700

    membenarkan model pegawai

--- application/models/Pegawai_model.php
+++ application/models/Pegawai_model.php
@@ -2,22 +2,20 @@
 
 class Pegawai_model extends CI_Model {
 	
-	private $_table = "pegawai" ;
-	public $id ;
-	public $nip ;
-	public $nama ;
+	private $_table = "admin" ;
+	public $id_admin ;
+	public $username ;	
 	public $password ;
-	public $gender ;
 	public $foto = "default.jpg";
 
 	public function rules () {
 		return [
-			['field' => 'nip' ,
-			'label' => 'NIP' ,
+			['field' => 'username' ,
+			'label' => 'username' ,
 			'rules' => 'required'] ,
 
-			['field' => 'nama' ,
-			'label' => 'Nama' ,
+			['field' => 'password' ,
+			'label' => 'password' ,
 			'rules' => 'required'] 
 			] ;
 	}
@@ -26,18 +24,16 @@ class Pegawai_model extends CI_Model {
 		return $this->db->get($this->_table)->result() ;
 	}
 
-	public function getById($id) {
-		return $this->db->get_where($this->_table , ["id"])->row() ;
+	public function getById($id_admin) {
+		return $this->db->get_where($this->_table , ["id_admin"])->row() ;
 	}
 
 	public function save() {
 		$post = $this->input->post() ;
 		// var_dump($post);
-		$this->nip = $post["nip"] ;
-		$this->nama = $post["nama"] ;
+		$this->id_admin = $post["id_admin"] ;
+		$this->username = $post["username"] ;
 		$this->password=md5($post["password"]) ;
-		// $this->password = $post["password"] ;
-		$this->gender = $post["gender"] ;
 		$this->foto = $this->_uploadImage();
 
 		$this->db->insert($this->_table , $this) ;
@@ -46,12 +42,10 @@ class Pegawai_model extends CI_Model {
 	public function update() {
 		$post = $this->input->post() ;
 		// var_dump($post);
-		$this->id = $post["id"] ;
-		$this->nip = $post["nip"] ;
-		$this->nama = $post["nama"] ;
+		$this->id_admin = $post["id_admin"] ;
+		$this->username = $post["username"] ;
 		$this->password=md5($post["password"]) ;
-		// $this->password = $post["password"] ;
-		$this->gender = $post["gender"] ;
+
 
 		if (!empty($_FILES["foto"]["name"])) {
             $this->foto = $this->_uploadImage();
@@ -60,20 +54,20 @@ class Pegawai_model extends CI_Model {
         }
 
 
-        $this->db->update($this->_table, $this, array("id" => $post["id"]));
+        $this->db->update($this->_table, $this, array("id_admin" => $post["id_admin"]));
     }	
 
-    public function delete($id)
+    public function delete($id_admin)
     {
-		$this->_deleteImage($id);
-        return $this->db->delete($this->_table, array("id" => $id));
+		$this->_deleteImage($id_admin);
+        return $this->db->delete($this->_table, array("id_admin" => $id_admin));
     }
 
     private function _uploadImage()
     {
         $config['upload_path']          = './foto/pegawai';
         $config['allowed_types']        = 'gif|jpg|png';
-        $config['file_name']            = $this->nama;
+        $config['file_name']            = $this->username;
         $config['overwrite']            = true;
         $config['max_size']             = 1024; // 1MB
         // $config['max_width']            = 1024;
@@ -88,9 +82,9 @@ class Pegawai_model extends CI_Model {
         return "default.jpg";
     }
 
-    private function _deleteImage($id)
+    private function _deleteImage($id_admin)
     {
-        $img = $this->getById($id);
+        $img = $this->getById($id_admin);
         if ($img->foto != "default.jpg") {
             $filename = explode(".", $img->foto)[0];
             return array_map('unlink', glob(FCPATH . "foto/pegawai/$filename.*"));
