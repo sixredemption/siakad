7bf145061828d2608fd20a1c0524e06181f1b4f6
unknown <akmalkhosy@gmail.com>
2019-04-23 22:00:52 +0700

    tambah model mapel, tahun, kelas, dan jurusan

---
+++ application/models/Model_jurusan.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Model_jurusan extends CI_Model
+{
+    private $_table = "jurusan";
+    public $id_jurusan;
+    public $nama_jurusan;
+
+    public function rules()
+    {
+        return [
+            [
+                'field' => 'nama_jurusan',
+                'label' => 'nama_jurusan',
+                'rules' => 'required'
+            ]
+        ];
+    }
+
+    public function getAll()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+
+    public function getById($id_jurusan)
+    {
+        return $this->db->get_where($this->_table, ["id_jurusan" => $id_jurusan])->row();
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        // $this->id_jurusan = $post["id_jurusan"];
+        $this->nama_jurusan = $post["nama_jurusan"];
+        
+        $this->db->insert($this->_table, $this);
+    }
+
+    public function update()
+    {
+        $post = $this->input->post();
+        //  var_dump($post);
+        $this->id_jurusan = $post["id_jurusan"];
+        $this->nama_jurusan = $post["nama_jurusan"];
+        
+        $this->db->update($this->_table, $this, array("id_jurusan" => $post["id_jurusan"]));
+    }
+
+    public function delete($id_jurusan)
+    {
+        $this->_deleteImage($id_jurusan);
+        return $this->db->delete($this->_table, array("id_jurusan" => $id_jurusan));
+    }
+
+}
---
+++ application/models/Model_mapel.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Model_mapel extends CI_Model
+{
+    private $_table = "mapel";
+    public $id_mapel;
+    public $nama_mapel;
+
+    public function rules()
+    {
+        return [
+            [
+                'field' => 'nama_mapel',
+                'label' => 'nama_mapel',
+                'rules' => 'required'
+            ]
+        ];
+    }
+
+    public function getAll()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+
+    public function getById($id_mapel)
+    {
+        return $this->db->get_where($this->_table, ["id_mapel" => $id_mapel])->row();
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        // $this->id_mapel = $post["id_mapel"];
+        $this->nama_mapel = $post["nama_mapel"];
+        
+        $this->db->insert($this->_table, $this);
+    }
+
+    public function update()
+    {
+        $post = $this->input->post();
+        //  var_dump($post);
+        $this->id_mapel = $post["id_mapel"];
+        $this->nama_mapel = $post["nama_mapel"];
+        
+        $this->db->update($this->_table, $this, array("id_mapel" => $post["id_mapel"]));
+    }
+
+    public function delete($id_mapel)
+    {
+        $this->_deleteImage($id_mapel);
+        return $this->db->delete($this->_table, array("id_mapel" => $id_mapel));
+    }
+
+}
