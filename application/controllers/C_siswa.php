bae6909cd68c31c91d8368525e1856bdd8ad35a1
dzikrialifa <dzikrialif99@gmail.com>
2019-04-24 17:58:59 +0700

    Edit Controllers Siswa

--- application/controllers/C_siswa.php
+++ application/controllers/C_siswa.php
@@ -7,13 +7,13 @@ class C_siswa extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("M_siswa");
+        $this->load->model("Model_siswa");
         $this->load->library('form_validation');
     }
 
     public function index()
     {
-        $data["siswa"] = $this->M_siswa->getAll();
+        $data["siswa"] = $this->Model_siswa->getAll();
         $this->load->view("template_admin/header");
         $this->load->view("admin/listsiswa", $data);
         $this->load->view("template_admin/sidebar");
@@ -24,7 +24,7 @@ class C_siswa extends CI_Controller
 
     public function add()
     {
-        $tambah = $this->M_siswa;
+        $tambah = $this->Model_siswa;
         $validation = $this->form_validation;
         $validation->set_rules($tambah->rules());
 
@@ -33,7 +33,7 @@ class C_siswa extends CI_Controller
             // $this->session->set_flashdata('success', 'Berhasil disimpan');
         }
 
-        $data["siswa"] = $this->M_siswa->getAll();
+        $data["siswa"] = $this->Model_siswa->getAll();
         $this->load->view("template_admin/header");
         $this->load->view("admin/listsiswa", $data);
         $this->load->view("template_admin/sidebar");
@@ -46,7 +46,7 @@ class C_siswa extends CI_Controller
         if (!isset($id)) redirect('c_siswa');
 
 
-        $var = $this->M_siswa;
+        $var = $this->Model_siswa;
         $validation = $this->form_validation;
         $validation->set_rules($var->rules());
 
@@ -55,7 +55,7 @@ class C_siswa extends CI_Controller
             $this->session->set_flashdata('success', 'Berhasil disimpan');
         }
 
-        // $data["siswa"]=$this->M_siswa->getAll();
+        // $data["siswa"]=$this->Model_siswa->getAll();
         // $this->load->view("template/header");
         // $this->load->view("admin/listsiswa", $data);
         // $this->load->view("template/sidebar");
@@ -73,8 +73,8 @@ class C_siswa extends CI_Controller
     {
         if (!isset($id)) show_404();
 
-        if ($this->M_siswa->delete($id)) {
-            $data["siswa"] = $this->M_siswa->getAll();
+        if ($this->Model_siswa->delete($id)) {
+            $data["siswa"] = $this->Model_siswa->getAll();
             $this->load->view("template_admin/header");
             $this->load->view("admin/listsiswa", $data);
             $this->load->view("template_admin/sidebar");
