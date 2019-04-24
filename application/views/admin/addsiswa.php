13871a6b2815edcbc0d75d460d66d709cdefa786
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-04-24 23:42:04 +0700

    final editing form addsiswa

--- application/views/admin/addsiswa.php
+++ application/views/admin/addsiswa.php
@@ -6,156 +6,89 @@
 				</a></li>
 				<li class="active">Dashboard</li>
 			</ol>
-        </div>
-                
-    <div class="container">
-		<div class="row mt-3">
-			<div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h1>Tambah Siswa</h1>
+        </div>      
+        <h1>Tambah Data Siswa</h1>
+        <div class="container-fluid">
+        <form action="<?php echo base_url(). 'Siswa/add'; ?>" method="post" enctype="multipart/form-data" >
+            
+            <div class="col-md-10">
+                <div class="form-group-input-group">
+                    <label for="nisn">Nisn </label>
+                    <input type="text" class="form-control" name="nis" placeholder="Nomor Induk Siswa">
                 </div>
+            </div>
 
-                
-<div class="card-body">
+            <div class="col-md-10">
+                <div class="form-group">
+                    <label for="nama_siswa">Nama</label>
+                    <input class="form-control <?php echo form_error('nama_siswa') ? 'is-invalid':'' ?>" type="text" name="nama_siswa" placeholder="Nama Siswa" />
+                <div class="invalid-feedback">
+                    <?php echo form_error('nama') ?>
+                </div>
+                </div>
+            </div>
 
-<form action="<?php echo base_url(). 'Siswa/add'; ?>" method="post" enctype="multipart/form-data" >
-        <div class="form-group-input-group">
-			<label for="nisn">Nisn </label>
-			<input type="text" class="form-control" name="nis" placeholder="Nomor Induk Siswa">
-		</div>
-        
-		<div class="form-group">
-			<label for="nama_siswa">Nama</label>
-			<input class="form-control <?php echo form_error('nama_siswa') ? 'is-invalid':'' ?>" type="text" name="nama_siswa" placeholder="Nama Siswa" />
-			<div class="invalid-feedback">
-			<?php echo form_error('nama') ?>
-			</div>
-		</div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="tanggal_lahir">Tanggal lahir</label>
+                    <input type="date" class="form-control" name="tanggal_lahir">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="kota"> Kota Asal</label>
+                    <input type="text" class="form-control" name="kota" placeholder="Kota Asal">
+                </div>
+            </div>
 
-        <div class="form-group">
-			<label for="tanggal_lahir">Tanggal lahir</label>
-			<input type="date" class="form-control" name="tanggal_lahir">
-		</div>
-
-        <div class="form-group">
-			<label for="kota"> Kota Asal</label>
-			<input type="text" class="form-control" name="kota" placeholder="Kota Asal">
-		</div>
-
-        <div class="form-group">
-            <label for="alamat">Alamat anda</label>
-            <textarea type="text" class="form-control" name="alamat"></textarea>
-        </div>
-
-        <div class="form-group">
-            <label for="Kelas">Kelas</label>
-                <select class="form-control" name="kelas">
-                    <option value="" disabled selected>pilih kelas</option>
-                    <option value="1">10-Ipa-1</option>
-                    <option value="2">10-Ipa-2</option>
-                    <option value="3">11-Ipa-1</option>
-                    <option value="4">11-Ipa-2</option>
-                    <option value="5">12-Ipa-1</option>
-                    <option value="6">12-Ipa-2</option>
-                    <option value="7">10-Ips-1</option>
-                    <option value="8">10-Ips-2</option>
-                    <option value="9">11-Ips-1</option>
-                    <option value="10">11-Ips-2</option>
-                    <option value="11">12-Ips-1</option>
-                    <option value="12">12-Ips-2</option>
-                    <option value="13">12-Ips-2</option>
-                    <option value="14">12-Ips-2</option>
-                    <option value="15">12-Ips-2</option>
-                    <option value="16">12-Ips-2</option>  
-                </select>
-         </div>
-
-        <!-- <div class="form-group">
-                <label for="nohp"> Nomer Hp</label>
-                <input type="number" class="form-control" name="no_telp" placeholder="Product name" placeholder="Nomor Hp">
-            </div> -->
-
-        <div class="form-group">
-        <label for="jenis_kelamin">Jenis Kelamin</label>
-        <input type="radio" name="jenis_kelamin" value="Laki-laki">Laki-Laki
-        <input type="radio" name="jenis_kelamin" value="Perempuan">Perempuann
-        </div>
-
-        <!-- <div class="form-group">
-            <label for="password">password</label>
-            <input type="password" class="form-control" name="password" placeholder="Password">
-        </div> -->
-
-         <!-- <div class="form-group">
-            <label for="Kelas">Jurusan</label>
-                <select class="form-control" name="id_jurusan">
-                    <option value="" disabled selected>Pilih Jurusan</option>
-                    <option value="1">IPA</option>
-                    <option value="2">IPS</option>
-
-                </select>
-         </div> -->
-
-        <div class="form-group">
-
-        <div class="form-group">
-            <label for="foto">Foto</label>
-            <input class="form-control-file" type="file" class="form-control" name="foto">
-        </div>
-
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="alamat">Alamat anda</label>
+                    <textarea class="form-control" name="alamat" rows="6"></textarea>
+                </div>
+            </div>
+                    
+            <div class="col-md-3">
+                    <div class="form-group">
+                        <label for="Kelas">Kelas</label>
+                            <select class="form-control" name="kelas">
+                                <option value="" disabled selected>pilih kelas</option>
+                                <option value="1">10-Ipa-1</option>
+                                <option value="2">10-Ipa-2</option>
+                                <option value="3">11-Ipa-1</option>
+                                <option value="4">11-Ipa-2</option>
+                                <option value="5">12-Ipa-1</option>
+                                <option value="6">12-Ipa-2</option>
+                                <option value="7">10-Ips-1</option>
+                                <option value="8">10-Ips-2</option>
+                                <option value="9">11-Ips-1</option>
+                                <option value="10">11-Ips-2</option>
+                                <option value="11">12-Ips-1</option>
+                                <option value="12">12-Ips-2</option>
+                                <option value="13">12-Ips-2</option>
+                                <option value="14">12-Ips-2</option>
+                                <option value="15">12-Ips-2</option>
+                                <option value="16">12-Ips-2</option>  
+                            </select>
+                    </div>
+                </div>
+            <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="jenis_kelamin">Jenis Kelamin</label>
+                        <input type="radio" name="jenis_kelamin" value="Laki-laki">Laki-Laki
+                        <input type="radio" name="jenis_kelamin" value="Perempuan">Perempuann
+                    </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <label for="foto">Foto</label>
+                    <input class="form-control-file" type="file" class="form-control" name="foto">
+                </div>
+            </div>
+            <div class="col-md-12"> 
             <button type="submit" class="btn btn-primary">Simpan</button>
             <button type="reset" class="btn btn-danger">Reset</button>
-    </form>
+        </form>
+    </div>
 </div>
-                </div>
-			</div>
-		</div>
-		<div class="form-group">
-			<label for="nama">Nama :</label>
-			<input type="text" class="form-control" id="nama">
-            </div>
-            <div class="form-group">
-			<label for="tanggallahir">Tanggal lahir:</label>
-			<input type="date" class="form-control" id="tanggallahir">
-		</div>
-        <div class="form-group">
-			<label for="kotaasal"> Kota Asal:</label>
-			<input type="text" class="form-control" id="kotaasal">
-		</div>	  
-        <div class="form-group">
-            <label for="alamat">Alamat anda:</label>
-            <textarea type="text" class="form-control" id="alamat"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="Kelas">Kelas:</label>
-        <select class="form-control">
-            <option value="" disabled selected>pilih kelas</option>
-            <option value="10ipa1">10-Ipa-1</option>
-            <option value="10ipa2">10-Ipa-2</option>
-            <option value="11ipa1">11-Ipa-1</option>
-            <option value="11ipa2">11-Ipa-2</option>
-            <option value="12ipa1">12-Ipa-1</option>
-            <option value="12ipa2">12-Ipa-2</option>
-            <option value="10ips1">10-Ips-1</option>
-            <option value="10ips2">10-Ips-2</option>
-            <option value="11ips1">11-Ips-1</option>
-            <option value="11ips2">11-Ips-2</option>
-            <option value="12ips1">12-Ips-1</option>
-            <option value="12ips2">12-Ips-2</option>  
-        </select>
-    </div>
-    <div class="form-group">
-			<label for="nohp"> Nomer Hp:</label>
-			<input type="number" class="form-control" id="kotaasal">
-		</div>	  
-    <div class="form-group">
-      <label for="jk">Jenis Kelamin:</label>
-      <input type="radio" name="gender" value="lakilaki">Laki-Laki
-      <input type="radio" name="gender" value="perempuan">Perempuann
-      </select>
-    </div>
-    <button type="submit" class="btn btn-primary">Simpan</button>
-    <button type="reset" class="btn btn-danger">Reset</button>
-
-  </div>
+		
