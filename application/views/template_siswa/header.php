1eb9e761108487ffb56e697a8d28035edb5d33ed
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-04-24 22:18:45 +0700

    memperbaiki tampilan sidebar

--- application/views/template_siswa/header.php
+++ application/views/template_siswa/header.php
@@ -1,5 +1,3 @@
-
-
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
@@ -7,6 +5,7 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="en">
 <!--<![endif]-->
+
 
 <head>
     <meta charset="utf-8">
