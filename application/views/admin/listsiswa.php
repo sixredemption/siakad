3aa7fb56670740eb3a20b23395028da6d0519897
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-04-25 14:01:18 +0700

    memperbaiki form list siswa

--- application/views/admin/listsiswa.php
+++ application/views/admin/listsiswa.php
@@ -8,17 +8,16 @@
 			</ol>
 		</div>
   
-		<div class= container>
+	<div class= container>
         <div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header">Daftar Siswa</h1>
-            <div class="panel-button-tab-left">
-				
-			<form method="" action="<?php  echo base_url(); ?>​. Admin/addsiswa;">
-       			<button id="submit-buttons" type="submit" ​​​​​>Tambah Data</button>
+			<div class="panel-button-tab-left">				
+			<form method="post"  action="<?php  echo base_url(). 'admin/addsiswa'; ?>">
+				<button class="btn btn-primary" id="submit-buttons" type="submit" ​​​​​>Tambah Pegawai</button>
 			</form>
-            </div>
-				<table class="table table-striped">
+			</div>	<br>
+			<table class="table table-striped">
 				<thead>
 					<tr>
 						<th><font face ="Calibri"> NO </font></th>
@@ -33,33 +32,33 @@
 						<th><font face ="Calibri"> foto </font></th>
 						<th><font face ="Calibri"> menu </font></th>
                     </tr>
-  			<tbody>
-				<?php  $nomor =1; ?>
-								<?php 
-								foreach($siswa  as $murid):
-								?>
-						<tr>
-							<td><?php echo $nomor; ?></td>
-							<td><p><?=  $murid->nisn?></p></td>
-							<td><p><?=  $murid->nama_siswa?></p></td>
-							<td><p><?=  $murid->tanggal_lahir?></p></td>
-							<td><p><?=  $murid->jenis_kelamin?></p></td>
-							<td><p><?=  $murid->id_kelas?></p></td>
-							<td><p><?=  $murid->kota?></p></td>
-							<td><p><?=  $murid->alamat?></p></td>
-							<td><p><?=  $murid->no_telp?></p></td>
-							<td>
-							<img src="<?php echo base_url('foto/siswa/'.$murid->foto) ?>" width="64" />
-							</td>
-							<td>
-							<?php echo anchor('Admin/siswaEdit/'.$murid->id_siswa,'Edit'); ?>
-                              <?php echo anchor('Admin/siswaDelete/'.$murid->id_siswa,'Hapus'); ?>
-							</td>
-						</tr>
-						<?php $nomor++; ?>
-						<?php endforeach; ?>
-					</tbody>	
-			</table>
+				<tbody>
+					<?php  $nomor =1; ?>
+									<?php 
+									foreach($siswa  as $murid):
+									?>
+							<tr>
+								<td><?php echo $nomor; ?></td>
+								<td><p><?=  $murid->nisn?></p></td>
+								<td><p><?=  $murid->nama_siswa?></p></td>
+								<td><p><?=  $murid->tanggal_lahir?></p></td>
+								<td><p><?=  $murid->jenis_kelamin?></p></td>
+								<td><p><?=  $murid->id_kelas?></p></td>
+								<td><p><?=  $murid->kota?></p></td>
+								<td><p><?=  $murid->alamat?></p></td>
+								<td><p><?=  $murid->no_telp?></p></td>
+								<td>
+								<img src="<?php echo base_url('foto/siswa/'.$murid->foto) ?>" width="64" />
+								</td>
+								<td>
+								<?php echo anchor('Admin/siswaEdit/'.$murid->id_siswa,'Edit'); ?>
+								<?php echo anchor('Admin/siswaDelete/'.$murid->id_siswa,'Hapus'); ?>
+								</td>
+							</tr>
+							<?php $nomor++; ?>
+							<?php endforeach; ?>
+						</tbody>	
+				</table>
 			</div>
 			<div>
 </div>
