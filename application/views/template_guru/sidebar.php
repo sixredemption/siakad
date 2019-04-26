e3bbf867b0e57d8fffe08ee57c03acaab071ca4b
debyprast <debyprast18@gmail.com>
2019-04-26 09:35:20 +0700

    menambah variasi di header

--- application/views/template_guru/sidebar.php
+++ application/views/template_guru/sidebar.php
@@ -127,4 +127,27 @@
                     </div>
                 </div>
             </div>
+            </header>
+
+            <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Menu</h1>
+                    </div>
+                </div>
+            </div>
+          <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li><a href="#">Dashboard</a></li>
+                            <li><a href="#">Table</a></li>
+                            <li class="active">Data table</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            </div>
+
 </body>
