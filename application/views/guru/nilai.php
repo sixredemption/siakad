2083257286b84030c3fa11af693f3c00ae3600b4
debyprast <debyprast18@gmail.com>
2019-04-26 10:39:43 +0700

    perbaikan tampilan nilai

--- application/views/guru/nilai.php
+++ application/views/guru/nilai.php
@@ -1,43 +1,61 @@
+<div class="content mt-3">
+<div class="animated fadeIn">
 
-<div class="col-sm-12">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="row">
-                <div class="col col-md-8">
-                    <i class="fa fa-external-link-square"></i>
-                    Upload Nilai
+
+<div class="row">
+
+    
+
+
+    <div class="col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Basic Form</strong> Elements
                 </div>
-        
-        <div class="panel-body">
-        <h2>Pilih nilai kelas yang akan diupload<h2>
-        
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="form-field-1">       
-                <select>
-                <option nama="Mapel" value="ipa"><a href="<?= base_url();?>ipa/guru">IPA</a></option>
-                <option nama="Mapel" value="ips"><a href="<?= base_url();?>ips/guru">IPS</a></option>
-                <option nama="Mapel" value="bi"><a href="<?= base_url();?>bi/guru">BI</a></option>
-                </select>
-                </label>
-            
-            <div class="form-group">
-                <label class="col-sm-3 control-label" for="form-field-1">       
-                <select>
-                <option nama="Mapel" value="ipa"><a href="<?= base_url();?>ipa/guru">Kelas 10</a></option>
-                <option nama="Mapel" value="ips"><a href="<?= base_url();?>ips/guru">Kelas 11</a></option>
-                <option nama="Mapel" value="bi"><a href="<?= base_url();?>bi/guru">Kelas 12</a></option>
-                </select>
-                </label>
-        
-            <div class="form-group">
-                <label class="col-sm-2 control-label" for="form-field-1">       
-                <select>
-                <option nama="Mapel" value="ipa"><a href="<?= base_url();?>ipa/guru">1</a></option>
-                <option nama="Mapel" value="ips"><a href="<?= base_url();?>ips/guru">2</a></option>
-                </select>
-                </label> 
+                <div class="card-body card-block">
+                    <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                    <div class="col col-md-2"><label for="select" class=" form-control-label">Jurusan</label></div>
+                    <div class="col-12 col-md-9">
+                        <select name="select" id="select" class="form-control">
+                            <option value="0">Jurusan</option>
+                            <option value="1">IPA</option>
+                            <option value="2">IPS</option>
+                        </select>
+                    </div>
+                </div>
 
-                <a <button type="button" class="btn btn-primary" href="<?= base_url();?>guru/isinilai">Tampilkan</a></button>      
-</section>
+                <div class="card-body card-block">
+                    <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                    <div class="col col-md-2"><label for="select" class=" form-control-label">Jurusan</label></div>
+                    <div class="col-12 col-md-9">
+                        <select name="select" id="select" class="form-control">
+                            <option value="0">Jurusan</option>
+                            <option value="1">IPA</option>
+                            <option value="2">IPS</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="card-body card-block">
+                    <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                    <div class="col col-md-2"><label for="select" class=" form-control-label">Jurusan</label></div>
+                    <div class="col-12 col-md-9">
+                        <select name="select" id="select" class="form-control">
+                            <option value="0">Jurusan</option>
+                            <option value="1">IPA</option>
+                            <option value="2">IPS</option>
+                        </select>
+                    </div>      
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary btn-sm">
+                        <i class="fa fa-dot-circle-o"></i> Submit
+                    </button>
+                    <button type="reset" class="btn btn-danger btn-sm">
+                        <i class="fa fa-ban"></i> Reset
+                    </button>
+                </div>      
+                </div>      
 </div>
-               
+       
