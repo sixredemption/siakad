e4851678df571b88e87d8477dcccef297ae3c96e
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-26 08:55:29 +0700

    membuat dan mengedit pada nilai_model.php

--- application/models/Nilai_model.php
+++ application/models/Nilai_model.php
@@ -11,9 +11,17 @@ class Nilai_model extends CI_Model {
 	public function setBatchImport($batchImport) {
 		$this->_batchImport = $batchImport ;
 	}
+	
 	// save data
 	public function importData() {
 		$data = $this->_batchImport ;
+		$this->db->insert_batch('C_nilai' , $data) ;
+	}
+
+	// get employee list
+	public function employeeList() {
+		$this->db->select(array('e.id_nilai' , 'e.id_guru' , 'e.id_kelas' , 
+		'e.id_siswa' , 'e.id_mapel' , 'e.nilai_siswa')) ;
 	}
 }
 
