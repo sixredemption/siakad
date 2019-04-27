0f7e477a90dfee28508fc393ea858ca71fde9447
dzikrialifa <dzikrialif99@gmail.com>
2019-04-27 15:46:26 +0700

    Edit Mapel Model

--- application/models/Model_mapel.php
+++ application/models/Model_mapel.php
@@ -3,14 +3,15 @@
 class Model_mapel extends CI_Model
 {
     private $_table = "mapel";
-    public $id_mapel, $nama_mapel;
+    public $id_mapel;
+    public $nama_mapel;
 
     public function rules()
     {
         return [
             [
                 'field' => 'nama_mapel',
-                'label' => 'Nama Mapel',
+                'label' => 'nama_mapel',
                 'rules' => 'required'
             ]
         ];
@@ -31,7 +32,7 @@ class Model_mapel extends CI_Model
         $post = $this->input->post();
         // $this->id_mapel = $post["id_mapel"];
         $this->nama_mapel = $post["nama_mapel"];
-
+        
         $this->db->insert($this->_table, $this);
     }
 
@@ -41,7 +42,7 @@ class Model_mapel extends CI_Model
         //  var_dump($post);
         $this->id_mapel = $post["id_mapel"];
         $this->nama_mapel = $post["nama_mapel"];
-
+        
         $this->db->update($this->_table, $this, array("id_mapel" => $post["id_mapel"]));
     }
 
@@ -49,4 +50,5 @@ class Model_mapel extends CI_Model
     {
         return $this->db->delete($this->_table, array("id_mapel" => $id_mapel));
     }
+
 }
