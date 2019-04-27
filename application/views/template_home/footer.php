cf55be7d836a0bbd7809ab877932d3f9e67f3988
dzikrialifa <dzikrialif99@gmail.com>
2019-04-27 15:49:00 +0700

    Edit Template Landing Page

--- application/views/template_home/footer.php
+++ application/views/template_home/footer.php
@@ -8,6 +8,25 @@
                             <div class="footer-logo">
                                 <h2><span>SMAN 4</span> MACIPO</h2>
                             </div>
+
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
+                            <div class="footer-icons">
+                                <ul>
+                                    <li>
+                                        <a href="#"><i class="fa fa-facebook"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-twitter"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-google"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -16,10 +35,13 @@
                     <div class="footer-content">
                         <div class="footer-head">
                             <h4>information</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                            </p>
                             <div class="footer-contacts">
-                                <p><span>Telepon :</span> (0321) 14045</p>
-                                <p><span>Email :</span> sman4macipo@yandex.com</p>
-                                <p><span>Jam Kerja :</span> 07.00 - 15.00</p>
+                                <p><span>Tel:</span> (0321) 14045</p>
+                                <p><span>Email:</span> sman4macipo@yandex.</p>
+                                <p><span>Jam Kerja:</span> 07.00 - 15.00</p>
                             </div>
                         </div>
                     </div>
@@ -37,6 +59,12 @@
                         </p>
                     </div>
                     <div class="credits">
+                        <!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
+              -->
                         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
                         Re-distributed by <a href="https://themewagon.com/" target="_blank">Themewagon</a>
                     </div>
