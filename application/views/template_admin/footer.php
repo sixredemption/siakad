0ffe7191b814ed13cde455a8ebad20b7ff78cb83
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-05-02 16:36:44 +0700

    memperbaiki footer

--- application/views/template_admin/footer.php
+++ application/views/template_admin/footer.php
@@ -4,6 +4,14 @@
 		</div><!--/.row-->
 	</div>	<!--/.main-->
 	
+	<!-- <script src="<?=base_url('assets_admin/js')?>/jquery-1.11.1.min.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/bootstrap.min.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/chart.min.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/chart-data.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/easypiechart.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/easypiechart-data.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/bootstrap-datepicker.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/custom.js"></script> -->
 	<script src="<?=base_url('assets_admin/js')?>/jquery-1.11.1.min.js"></script>
 	<script src="<?=base_url('assets_admin/js')?>/bootstrap.min.js"></script>
 	<script src="<?=base_url('assets_admin/js')?>/chart.min.js"></script>
@@ -11,9 +19,31 @@
 	<script src="<?=base_url('assets_admin/js')?>/easypiechart.js"></script>
 	<script src="<?=base_url('assets_admin/js')?>/easypiechart-data.js"></script>
 	<script src="<?=base_url('assets_admin/js')?>/bootstrap-datepicker.js"></script>
-	<script src="<?=base_url('assets_admin/js')?>/custom.js"></script>
+	<script src="<?=base_url('assets_admin/js')?>/bootstrap-table.js"></script>
+	
+
 	<script>
-		window.onload = function () {
+		!function ($) {
+			$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
+				$(this).find('em:first').toggleClass("glyphicon-minus");	  
+			}); 
+			$(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+		}(window.jQuery);
+
+		$(window).on('resize', function () {
+		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+		})
+		$(window).on('resize', function () {
+		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+		})
+	</script>
+
+
+
+
+
+	<script>
+ 		window.onload = function () {
 	var chart1 = document.getElementById("line-chart").getContext("2d");
 	window.myLine = new Chart(chart1).Line(lineChartData, {
 	responsive: true,
