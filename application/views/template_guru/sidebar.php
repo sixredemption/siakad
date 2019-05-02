7879eff56026d91b21f5d10ab81f2022c6e042fe
debyprast <debyprast18@gmail.com>
2019-05-02 21:39:10 +0700

    penghapusan fitur search di sidebar

--- application/views/template_guru/sidebar.php
+++ application/views/template_guru/sidebar.php
@@ -38,13 +38,7 @@
                 <div class="col-sm-6">
                     <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
                     <div class="header-left">
-                        <button class="search-trigger"><i class="fa fa-search"></i></button>
-                        <div class="form-inline">
-                            <form class="search-form">
-                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
-                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
-                            </form>
-                        </div>
+                    
 
                         <div class="dropdown for-notification">
                             <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
