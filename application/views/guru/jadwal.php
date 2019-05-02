5202e786547b48eb697ddd133b6557ba02363722
debyprast <debyprast18@gmail.com>
2019-05-02 22:24:32 +0700

    perubahan tampilan jadwal

--- application/views/guru/jadwal.php
+++ application/views/guru/jadwal.php
@@ -9,12 +9,7 @@
       </div>
 
         <div class="panel-body">
-          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPA Kelas 10</i></button>
-          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPS Kelas 10</i></button>
-          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPA Kelas 11</i></button>
-          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPS Kelas 11</i></button>
-          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPA Kelas 12</i></button>
-          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> IPS Kelas 12</i></button>
+          <button type="button" class="btn btn-primary"><i class="fa fa-tags"> Jadwal</i></button>
         </div>
 </section> 
 </div>
