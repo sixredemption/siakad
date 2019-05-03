de2376c05065223d875dba2ef350ad07e552ff0a
AshfinRamadhandy <ashfinramadhandy10@gmail.com>
2019-05-03 08:05:57 +0700

    memberbaiki controller home

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/siakad/';
+$config['base_url'] = 'http://localhost:100/siakad/';
 /*
 |--------------------------------------------------------------------------
 | Index File
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -32,7 +32,6 @@ class Home extends CI_Controller
 		}
 	}	
 
-	}
 
 	public function pengumuman()
 	{
