347f9dfe48be88a9bfa53065a13312ef87830fe6
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-05-07 13:48:33 +0700

    ping

--- application/views/template_admin/header.php
+++ application/views/template_admin/header.php
@@ -20,7 +20,7 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
 		<div class="container-fluid">
 			<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse"><span class="sr-only">Toggle navigation</span>
