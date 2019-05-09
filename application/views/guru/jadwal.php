ac70570df5a6f6b0996dc11a5bbb98f488b1ff81
debyprast <debyprast18@gmail.com>
2019-05-09 10:46:26 +0700

    penataan code jadwal

--- application/views/guru/jadwal.php
+++ application/views/guru/jadwal.php
@@ -1,38 +1,39 @@
 <div class="container">
-<section class="content">
+	<section class="content">
 
-<div class="breadcrumbs">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Menu</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="#">Menu</a></li>
-                            <li class="active">Jadwal</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<div class="breadcrumbs">
+								<div class="col-sm-4">
+										<div class="page-header float-left">
+												<div class="page-title">
+														<h1>Menu</h1>
+												</div>
+										</div>
+								</div>
+							<div class="col-sm-8">
+									<div class="page-header float-right">
+											<div class="page-title">
+												<ol class="breadcrumb text-right">
+														<li><a href="#">Menu</a></li>
+														<li class="active">Jadwal</li>
+												</ol>
+										</div>
+							</div>
+					</div>
+		</div>
 				
-<div class="jumbotron text-center"> 
-  <h3> Download Jadwal </h3>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-      </div>
+				<div class="jumbotron text-center"> 
+					<h3> Download Jadwal </h3>
+				<div class="panel panel-default">
+				<div class="panel-heading">
+     
 
         <div class="panel-body">
           <button type="button" class="btn btn-primary"><i class="fa fa-tags"> Kelas 10</i></button>
 					<button type="button" class="btn btn-primary"><i class="fa fa-tags"> Kelas 11</i></button>
 					<button type="button" class="btn btn-primary"><i class="fa fa-tags"> Kelas 12</i></button>
-				</div>				
-</section> 
+				</div>	
+
+	</section> 
 </div>
 
 
