4b2dc4582e6e0d5ca804386242b6c8391adc9672
AlfalahAnugara <alfalah190299@gmail.com>
2019-05-17 08:39:20 +0700

    menambahkan file My_profile.php untuk melakukan update Profile guru

---
+++ application/core/My_Profile.php
@@ -0,0 +1,15 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class My_Profile extends CI_Controller {
+
+	public function updateProfile() {
+
+		if ($this->userdata != '') {
+			$data = $this->model_Profileguru->select($this->userdata->id) ;
+
+			$this->session->set_userdata('userdata' , $data) ;
+			$this->userdata = $this->session->userdata('userdata') ;
+		}
+	}
+
+}
