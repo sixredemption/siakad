e4b4b98e17d7bd098cdab0beb3d1d9c0d943dac9
debyprast <debyprast18@gmail.com>
2019-05-17 09:02:37 +0700

    merubah base url

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ $config['composer_autoload'] = 'vendor/autoload.php' ;
 |
 */
 
-$config['base_url'] = 'http://localhost/siakad/';
+$config['base_url'] = 'http://localhost:8080/siakad/';
 
 /*
 |--------------------------------------------------------------------------
