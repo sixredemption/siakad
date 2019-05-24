878411c25e4f2df1e0aaa1c1ee725beee68f654a
unknown <akmalkhosy@gmail.com>
2019-05-24 08:13:40 +0700

    edit value semester

--- application/views/admin/addformspp.php
+++ application/views/admin/addformspp.php
@@ -47,10 +47,6 @@
                        <option value="" disabled selected>pilih semester</option>
                        <option value ="1">1</option>
                        <option value ="2">2</option>
-                       <option value ="3">3</option>  
-                       <option value ="4">4</option>  
-                       <option value ="5">5</option>  
-                       <option value ="6">6</option>      
                    </select>    
                </div>
            </div>
