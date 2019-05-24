fd75c9db3130d20c2f5695e32344fb9e9c0b926f
dzikrialifa <dzikrialif99@gmail.com>
2019-05-24 08:08:24 +0700

    Edit Landing Page

--- application/views/template_home/index.php
+++ application/views/template_home/index.php
@@ -501,35 +501,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <!-- Start  contact -->
-                    <div class="col-md-8 col-md-offset-2 col-sm-6 col-xs-12">
-                        <div class="form contact-form">
-                            <div id="sendmessage">Your message has been sent. Thank you!</div>
-                            <div id="errormessage"></div>
-                            <form action="" method="post" role="form" class="contactForm">
-                                <div class="form-group">
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Nama anda" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="email@gmail.com" data-rule="email" data-msg="Please enter a valid email" />
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="form-group">
-                                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Pesan" placeholder="Message"></textarea>
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="text-center"><button type="submit">Kirim</button></div>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- End Left contact -->
-                </div>
             </div>
         </div>
     </div>
